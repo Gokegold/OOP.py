d5478853e9d1bd8efa00f3871799e2ec6f9bc1da
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-28 00:58:32 +0100

    Update parentclass.py

--- parentclass.py
+++ parentclass.py
@@ -1,3 +1,5 @@
 # Remember, to create a child class,
 #you create new class with its own name and then put the name of the parent class in parentheses.
 #Add the following to the dog.py file to create three new child classes of the Dog class:
+# May 27, 2023
+# May 28, 2023
