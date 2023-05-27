67875060cc7d0e465ed90f6c951813c7ec83f2f0
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-27 12:49:14 +0100

    Create parentclass.py

---
+++ parentclass.py
@@ -0,0 +1,3 @@
+# Remember, to create a child class,
+#you create new class with its own name and then put the name of the parent class in parentheses.
+#Add the following to the dog.py file to create three new child classes of the Dog class:
